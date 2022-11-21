296551d31463d0b418c30bc6c3b01cf0ffd6ef66
Respius <59863735+Respius@users.noreply.github.com>
2022-11-21 10:49:11 +0300

    Add all my files

---
+++ about.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">HTC Africa</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="service.php">Service</a>
+      <a href="contact.php">Contact</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/logo.png" alt="">
+ 
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p>Communication Technology (ICT) Digital Security, Fleet and Fuel System Management and Fiber Optic and Telecommunication Solutions.<br/> The company has built up an
+      enviable reputation for providing innovative and cost-effective ICT and Security Solutions.</br> Our ability and
+      comprehensive experience means we can provide complete ICT & Security solutions for companies of every
+      size and sector, across the Tanzania market..</p><br/><br/><br/><br/>
+
+   </div>
+
+   <div class="content2">
+      <h1>Our Vision</h1>
+      <p>To become the most successful and respected Digital System intergrator company in Africa.</p><br/><br/>
+      
+      <h1>Our Mission</h1>
+      <p>To be the most innovative and cost-efficient provider of Digital System Solutions, while motivating our 
+         employees constantly to excel and to be dynamic.</p><br/>
+      
+      <h1>Our Values</h1>
+      <p>As a company, and as an individual, we value the importance of time, integrity, openness, personal excellence, constructive selfcriticism,
+         continual self-improvement, and mutual respect. We are committed to our customers and
+         partners and have a passion for technology. We take on big challenges, and pride ourselves on seeing
+         them through. We hold ourselves accountable to our customers, shareholders, partners, and employees
+         by honoring our commitments, providing results, and striving for the highest quality.
+      </p>   
+
+   </div>
+   
+   <div class="content">
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading-title"> clients reviews </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p></p>
+            <h3>Qatar Embassy</h3>
+            <span>Tanzania</span>
+            <img src="images/state-of-qatar-logo.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p></p>
+            <h3>CRDB Bank</h3>
+            <span></span>
+            <img src="images/crdb-bank.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p></p>
+            <h3>Twaweza</h3>
+            <span>NGO</span>
+            <img src="images/twaweza.jpg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p></p>
+            <h3>Regus</h3>
+            <span>Tanzania</span>
+            <img src="images/regus.jpg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Africa Flight Service</h3>
+            <span></span>
+            <img src="images/afs.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Cats-net</h3>
+            <span></span>
+            <img src="images/cats.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Ethiopia Embassy</h3>
+            <span></span>
+            <img src="images/ethiopia.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Securex</h3>
+            <span></span>
+            <img src="images/securex.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Total</h3>
+            <span></span>
+            <img src="images/total.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Mkombozi Bank</h3>
+            <span></span>
+            <img src="images/mkombozi.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Maisha Supermarket</h3>
+            <span></span>
+            <img src="images/maisha.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Village Supermarket</h3>
+            <span></span>
+            <img src="images/village.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Pioneer Builders</h3>
+            <span></span>
+            <img src="images/pioneer.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+
+            </div>
+            <p</p>
+            <h3>Alshifa Hospital</h3>
+            <span></span>
+            <img src="images/alshifa.png" alt="">
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="service.php"> <i class="fas fa-angle-right"></i> service</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <a href="#"> <i class="fas fa-phone"></i> +255-752-795471 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +255-745-257265 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> account@htc.co.tz </a>
+         <a href="#"> <i class="fas fa-map"></i> Mbezi Beach, Dar es salaam-  </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+  <div class="credit">  <span>HTC Africa</span> | &copy; all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ service.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Service</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">HTC africa</a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="service.php">Service&Solution</a>
+      <a href="contact.php">Contact</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.jpg) no-repeat">
+   <h1>Service&Solution</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">Services and Solutions we offer</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Digital Conference System</h3>
+            <p>
+               Wireless control<br>
+               Timing Speaking<br>
+               SM Notice<br>
+               Tracking Camera<br>
+               Independent EQ adjustment<br>
+            </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>WI-FI Solutions</h3>
+            <p>
+               Enterprise Wi-Fi solution <br>
+               Small Office Wi-Fi solution<br>
+               Residential Wi-Fi solution<br>
+            </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Tower Space Lease</h3>
+            <p>               
+               2Mt or more Space<br>
+               Power & Stand by Generator <br>
+               Technical Support <br>
+            </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Data and VOIP</h3>
+            <p>
+               Resources sharing
+               - Software
+               - Printers
+               - Copiers
+               - Scanners
+               -File sharing
+               Wireless control<br>
+               Timing Speaking<br>
+               SM Notice<br>
+               Tracking Camera<br>
+               Independent EQ adjustment<br>
+            </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>IP Intercom System</h3>
+            <p>Cloud-based Architecture<br>
+               Wireless Technique Air Play<br>
+               Latest Communication Technique with Super High Confidentiality<br>
+               SM Public Address<br>
+               Mange via Wifi<br>
+               Offline Public Address
+            </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Softwre and Website Design</h3>
+            <p>Hotel Management Software<br>
+               School Management software
+               Inventory Management Software<br>
+               Tours Website
+            </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/light.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Audio Visual and Light Intergrated Slolutions</h3>
+            <p>Wall Light
+               Flood Light
+               Corrugated Light
+               Window Light
+               Corner Light
+               Strip Light
+               Point Light Source
+               Fountain Light
+               Wall Washer
+               Underwater Light
+               Underground Light
+               Tunnel Light
+               Street Light
+            </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/firealarm.png" alt="">
+         </div>
+         <div class="content">
+            <h3>Fire Alarm Systems</h3>
+            <p></p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Paperless Conference System</h3>
+            <p>Pre-session function
+               Conference establishment
+               Conference preset
+               Personnel management
+               Theme management
+               Voting Management<br>
+               Function in meeting
+               Sign in
+               File distribution
+               Browsing
+               Document annotation
+               File upload
+               Vote
+               Signal interaction
+               Conference communication
+               Chairman functionConference notice
+               Backgstage control
+               </p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Backup installation and maintenance</h3>
+            <p></p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3></h3>
+            <p></p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3></h3>
+            <p</p>
+            <a href="contact.php" class="btn">View Details</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="service.php"> <i class="fas fa-angle-right"></i> service</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +255-752-795471 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +255-745-257265 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> account@htc.co.tz </a>
+         <a href="#"> <i class="fas fa-map"></i> Mbezi Beach, Dar es salaam-  </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit">  <span>HTC Africa</span> | &copy; all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
